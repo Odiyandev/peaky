ebb3d0eace80b061c202ce4f1d520b7995bd54aa
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2022-07-21 22:34:25 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -26,6 +26,7 @@ scheduler = AsyncIOScheduler()
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     global ti
+    global k
     id = message.from_user.id
     user_name = '@' + message.from_user.username if message.from_user.username else None
     try:
@@ -82,12 +83,12 @@ async def start_command(client: Client, message: Message):
                 reply_markup = None
 
             try:                
-                k=await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
+                k = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
                 dl_ids.append(k.id)
                 await asyncio.sleep()
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                k=await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
+                k = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
                 dl_ids.append(k.id)
             except:
                 pass
